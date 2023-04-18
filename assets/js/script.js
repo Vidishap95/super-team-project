0433bb9b907712609439bf602e7edce71fc4b1b7
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-18 14:01:45 -0400

    save changes before main pull down

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("Hello!");
 
 var posterURL = "https://image.tmdb.org/t/p/w342/1yDBZiMSVGS1IWWb7B2Rwjdz0Zj.jpg";
 var title = "Sixteen Candles";
-var imdbRating = 70/10;
+var imdbRating = "7.0";
 var year = "1984";
 var description = "A teenage girl deals with her parents forgetting her birthday and a crush on her high school's heartthrob.";
 var youtubeTrailerVideoLink = "https://www.youtube.com/watch?v=FDQu9IiPbOM";
@@ -10,8 +10,13 @@ var movieStreaming = "Netflix";
 
 var moviePosterEL = document.getElementById('movie-poster');
 var movieTitleEl = document.getElementById('movie-title');
-var movieDataEL = document.getElementById('movie-data');
+var movieInfoEL = document.getElementById('movie-info');
 
 moviePosterEL.src = posterURL;
 movieTitleEl.textContent = title;
-document.getElementById('movie-data').innerHTML = "Rating: " + imdbRating + "<br><br>" + year + "<br><br>" + description + "<br></br>" + youtubeTrailerVideoLink + "<br><br>" + movieStreaming;
+
+movieInfoEL.children[1].textContent = (year);
+movieInfoEL.children[2].textContent = ("IDMb "+ imdbRating);
+movieInfoEL.children[3].textContent = (description);
+movieInfoEL.children[4].textContent = ("youtube trailer for " + title);
+movieInfoEL.children[4].href = (youtubeTrailerVideoLink);
