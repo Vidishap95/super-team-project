0401d0e84c012f69c86b94dda4b6f8c8248277dc
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-19 16:02:21 -0400

    got object key and value link working for streaming platforms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 //API Key for "Streaming Availabilty API"  ** only 100 calls/day **
 var apiKey = 'c57e08632cmshdf60e809c2ea152p100330jsn3e0a705522cf';
 var apiKeyM = 'd6f6f25352msh9dd6753ebd9249ep1e97c8jsnb8f2487ea3cd';
+var apiKeyC = 'ea2834bc81msh2d97ced269ee288p19940ejsnff1f27caaa7d';
 
 //data from the API used to fetch the data
 var options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': apiKeyM,
+        'X-RapidAPI-Key': apiKeyC,
         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
     }
 };
@@ -43,7 +44,7 @@ function getMovieDetails(dataObject){
    var year = dataObject.year;
    var description = dataObject.overview;
    var youtubeTrailerVideoLink = dataObject.youtubeTrailerVideoLink
-   var movieStreaming = dataObject.streamingInfo.us //still working on this!
+ 
 
    var moviePosterEL = document.getElementById('movie-poster');
    var movieTitleEl = document.getElementById('movie-title');
@@ -57,10 +58,26 @@ function getMovieDetails(dataObject){
    movieInfoEL.children[3].textContent = (description);
    movieInfoEL.children[4].textContent = ("Watch the youTube trailer");
    movieInfoEL.children[4].href = (youtubeTrailerVideoLink);
-   // movieInfoEL.children[5].textContent = ("<br>Hulu: " + movieStreaming.hulu[0].link); //needs work - will break code as of right now!!
-   // movieInfoEL.children[5].href = (movieStreaming.hulu[0].link); //needs work - will break code as of right now!!
-}
 
+
+   
+   
+   var movieStreaming = dataObject.streamingInfo.us;//still working on this!
+   console.log(movieStreaming);
+   var streamingObjectKeys = Object.keys(movieStreaming);
+   console.log(streamingObjectKeys);
+
+   for (var i=0; i < streamingObjectKeys.length; i++){
+      movieInfoEL.children[5].textContent = streamingObjectKeys[i]; //needs work - will break code as of right now!!
+      // movieInfoEL.children[5].href = (movieStreaming.prime[0].link); //needs work - will break code as of right now!!
+      var businessKey = streamingObjectKeys[i]; //prime
+      var businessValue = movieStreaming[businessKey];
+      var businessLink = businessValue[0].link;
+      console.log(businessKey);
+      console.log(businessLink);
+   }
+}
+// + movieStreaming.hulu[0].link
 //console.log("Welcome to our page");
 
 
