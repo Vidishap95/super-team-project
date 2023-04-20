b1a1e2a6365ddbfb2aa3e77a1400db91d5c19570
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-19 22:01:59 -0400

    streaming services listed on webpage with links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 //API Key for "Streaming Availabilty API"  ** only 100 calls/day **
 var apiKey = 'c57e08632cmshdf60e809c2ea152p100330jsn3e0a705522cf';
 var apiKeyM = 'd6f6f25352msh9dd6753ebd9249ep1e97c8jsnb8f2487ea3cd';
-var apiKeyC = 'ea2834bc81msh2d97ced269ee288p19940ejsnff1f27caaa7d';
 
 //data from the API used to fetch the data
 var options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': apiKeyC,
+        'X-RapidAPI-Key': apiKeyM,
         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
     }
 };
@@ -40,44 +39,38 @@ movieButton.addEventListener('click', function(event){
 function getMovieDetails(dataObject){
    var posterURL = dataObject.posterURLs['342'];
    var title = dataObject.title;
-   var imdbRating = dataObject.imdbRating;
+   var imdbRating = dataObject.imdbRating/10;
    var year = dataObject.year;
    var description = dataObject.overview;
    var youtubeTrailerVideoLink = dataObject.youtubeTrailerVideoLink
- 
+   var movieStreaming = dataObject.streamingInfo.us;
+   var streamingObjectKeys = Object.keys(movieStreaming);
 
    var moviePosterEL = document.getElementById('movie-poster');
    var movieTitleEl = document.getElementById('movie-title');
    var movieInfoEL = document.getElementById('movie-info');
+   var streamListEL = document.getElementById('movie-streaming');
 
    moviePosterEL.src = posterURL;
    movieTitleEl.textContent = title;
-
    movieInfoEL.children[1].textContent = (year);
    movieInfoEL.children[2].textContent = ("IDMb "+ imdbRating);
    movieInfoEL.children[3].textContent = (description);
-   movieInfoEL.children[4].textContent = ("Watch the youTube trailer");
+   movieInfoEL.children[4].textContent = ("youTube Trailer");
    movieInfoEL.children[4].href = (youtubeTrailerVideoLink);
 
-
-   
-   
-   var movieStreaming = dataObject.streamingInfo.us;//still working on this!
-   console.log(movieStreaming);
-   var streamingObjectKeys = Object.keys(movieStreaming);
-   console.log(streamingObjectKeys);
-
    for (var i=0; i < streamingObjectKeys.length; i++){
-      movieInfoEL.children[5].textContent = streamingObjectKeys[i]; //needs work - will break code as of right now!!
-      // movieInfoEL.children[5].href = (movieStreaming.prime[0].link); //needs work - will break code as of right now!!
-      var businessKey = streamingObjectKeys[i]; //prime
-      var businessValue = movieStreaming[businessKey];
-      var businessLink = businessValue[0].link;
-      console.log(businessKey);
-      console.log(businessLink);
+      var platformKey = streamingObjectKeys[i];
+      var platformLink = movieStreaming[platformKey][0].link;
+
+      var list = document.createElement("li");
+      var listAnchor = document.createElement("a");
+
+      listAnchor.textContent = platformKey;
+      listAnchor.setAttribute("href", platformLink);
+      listAnchor.setAttribute("target", "_blank");
+
+      streamListEL.appendChild(list);
+      list.appendChild(listAnchor);
    }
 }
-// + movieStreaming.hulu[0].link
-//console.log("Welcome to our page");
-
-
